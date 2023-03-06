c8b326fe727a40b0e266e4d95fc3e00d5df104fd
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-03-06 09:39:12 +0100

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import NavBar from "./components/NavBar"
 import Home from "./pages/Home/Home"
 import ErrorPage from "./pages/ErrorPage"
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar />
         <Routes>
           <Route path="/" element={<Home />}/>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import FirstPart from '../../components/Custom/FirstPart'
 import QuizPart from '../../components/Custom/QuizPart'
 import LastPart from '../../components/Custom/LastPart'
+import NavBar from '../../components/NavBar'
 
 const Home = () => {
     return (
         <div className='bg-gray-100'>
+            <NavBar />
             <FirstPart />
             <QuizPart />
             <LastPart />
